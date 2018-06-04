3cd06fb8ba2f57e4364b6cb815bb32ee4fc63cbd
Michael Van Brummen <michaelvanbrummen@gmail.com>
2018-06-04 17:14:36 +1000

    Added principal username to repository page

--- src/main/kotlin/mvanbrummen/gitforge/controller/RepositoryController.kt
+++ src/main/kotlin/mvanbrummen/gitforge/controller/RepositoryController.kt
@@ -5,18 +5,21 @@ import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import java.security.Principal
 import javax.servlet.http.HttpServletRequest
 
 @Controller
 class RepositoryController(private val repositoryService: RepositoryService) {
 
     @GetMapping("/repo/{accountName}/{repoName}")
-    fun repositoryPage(@PathVariable accountName: String, @PathVariable repoName: String, model: Model): String {
+    fun repositoryPage(@PathVariable accountName: String, @PathVariable repoName: String, model: Model,
+                       principal: Principal): String {
         val repository = repositoryService.getRepositorySummary(accountName, repoName)
         model.addAttribute("repository", repository)
         model.addAttribute("repoName", repoName)
         model.addAttribute("branches", repository.repoSummary.branches)
         model.addAttribute("tags", repository.repoSummary.tags)
+        model.addAttribute("username", principal.name)
         return "repository"
     }
 
