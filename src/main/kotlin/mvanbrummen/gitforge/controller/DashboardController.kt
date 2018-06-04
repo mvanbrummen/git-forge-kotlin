c552784228b99eca5c5dfe68f90b5ce93c200db0
Michael Van Brummen <michaelvanbrummen@gmail.com>
2018-06-04 16:00:50 +1000

    Added principal username to dashboard

--- src/main/kotlin/mvanbrummen/gitforge/controller/DashboardController.kt
+++ src/main/kotlin/mvanbrummen/gitforge/controller/DashboardController.kt
@@ -4,14 +4,15 @@ import mvanbrummen.gitforge.service.RepositoryService
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
+import java.security.Principal
 
 @Controller
 class DashboardController(val repositoryService: RepositoryService) {
 
     @GetMapping("/")
-    fun dashboardPage(model: Model): String {
-        model.addAttribute("repos", repositoryService.findByRepositoriesByAccount("mvanbrummen"))
-
+    fun dashboardPage(principal: Principal, model: Model): String {
+        model.addAttribute("repos", repositoryService.findByRepositoriesByAccount(principal.name))
+        model.addAttribute("username", principal.name)
         return "dashboard"
     }
 }
